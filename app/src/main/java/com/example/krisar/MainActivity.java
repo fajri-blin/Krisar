1b88d7debc76673ac9b03aa166ffc478046ab11c
fajri-blin <fajriindonesia1945@gmail.com>
2019-12-26 08:31:33 +0700

    Sudah membuat view input krisar

--- app/src/main/java/com/example/krisar/MainActivity.java
+++ app/src/main/java/com/example/krisar/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.krisar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "Firebase Connection Sucessfull", Toast.LENGTH_SHORT).show();
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
     }
 }
---
+++ app/src/main/java/com/example/krisar/user.java
@@ -0,0 +1,15 @@
+package com.example.krisar;
+
+public class user {
+    public String username;
+    public String password;
+
+    public user(){
+
+    }
+
+    public user(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}

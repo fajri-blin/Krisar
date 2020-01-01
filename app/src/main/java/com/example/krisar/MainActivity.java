2b52d55b536b90f4b5f4fd62bf164d6e0e5f1e0d
fajri-blin <fajriindonesia1945@gmail.com>
2020-01-01 22:42:08 +0700

    All Sistem Login Logout & Register Sucess

--- app/src/main/java/com/example/krisar/MainActivity.java
+++ app/src/main/java/com/example/krisar/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.krisar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+
+        if(id==R.id.add_menu){}
+        if(id==R.id.logout){
+            FirebaseAuth.getInstance().signOut();
+            Intent Login = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(Login);
+            Toast.makeText(MainActivity.this,"Logout Success", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
